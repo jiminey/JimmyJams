0dfc5071bb2d166b53e3084cfd7ab9031d9eb0b3
jiminey <jtwnguyen@ucdavis.edu>
2019-07-05 10:31:58 -0700

    Remove song.rb to deploy to heroku

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,2 @@
 class Song < ApplicationRecord
-
-    validates: :title, :artist, presence: true
-
-    belongs_to :uploader,
-    primary_key: :id, 
-    foreign_key: :uperloader_id,
-    class_name: 
-
 end
