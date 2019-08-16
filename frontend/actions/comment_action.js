cbaea55e49501d852ec879fc28fbd5e703afab6a
jiminey <jtwnguyen@ucdavis.edu>
2019-08-16 13:43:00 -0700

    Add util and action for comments

---
+++ frontend/actions/comment_action.js
@@ -0,0 +1,30 @@
+import * as APICommentUtil from '../util/comment_api_util';
+
+export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+
+const receiveComment = comment => ({
+    type: RECEIVE_COMMENT,
+    comment
+});
+
+const removeComment = commentId => ({
+    type: REMOVE_COMMENT,
+    commentId
+});
+
+export const createComment = (comment, songId) => dispatch => (
+    APICommentUtil.createComment(comment, songId).then(comment => dispatch(receiveComment(comment)))
+);
+
+export const deleteComment = commentId => dispatch => (
+    APICommentUtil.deleteComment(commentId).then(commentId => dispatch(removeComment(commentId)))
+);
+
+
+
+
+
+
+
+)
