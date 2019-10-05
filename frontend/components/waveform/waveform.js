79e118adc5d4e7e71a44cfe9a58c2970e3652ed2
jiminey <jtwnguyen@ucdavis.edu>
2019-10-04 22:00:29 -0700

    Create waveform file

---
+++ frontend/components/waveform/waveform.js
@@ -0,0 +1,24 @@
+
+class WaveForm {
+
+    constructor(canvas, context, song, currentSong, currentAudio) {
+        this.ctx = context;
+        this.cvs = canvas;
+        this.song = song;
+        this.currentSong = currentSong;
+        this.currentAudio = currentAudio;
+    }
+
+
+
+    draw() {
+
+    }
+
+    update() {
+
+    }
+}
+
+
+export default WaveForm
