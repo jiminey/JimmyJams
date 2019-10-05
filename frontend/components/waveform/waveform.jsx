6c38fa5112f2c640189a72a520b4927495e721f1
jiminey <jtwnguyen@ucdavis.edu>
2019-10-04 22:51:50 -0700

    Got page to load

--- frontend/components/waveform/waveform.jsx
+++ frontend/components/waveform/waveform.jsx
@@ -1,20 +1,54 @@
 import React from 'react'
-
+import WaveSurfer from 'wavesurfer.js'
 
 class WaveForm extends React.Component {
     constructor(props) {
         super(props)
 
-        this.waveRef = React.createRef();
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
         this.state = {
-            loading: true,
-            width: 800,
+            width: 600,
             loaderPosition: 0,
-            loadingDirection: -1,
         }
     }
+
+
+    componentDidMount() {
+        const wave = this.refs.waveform;
+        this.wavesurfer = WaveSurfer.create({
+            container: wave,
+            waveColor: '#F2F2F2',
+            progressColor: '#F65502',
+            barWidth: 2,
+            height: 200,
+            fillParent: true,
+            cursorWidth: 0,
+            interact: true,
+            autoCenter: true,
+            closeAudioContext: true,
+            hideScrollbar: true,
+            partialRender: true,
+            removeMediaElementOnDestroy: true,
+        });
+
+        this.wavesurfer.load(this.props.song);
+
+        this.wavesurfer.on('ready', function () {
+            wavesurfer.play();
+        });
+        
+    }
+
+    render() {
+        return(
+            <div id='waveform' className='waveform' ref='waveform'>
+                <div className="outer-loader">
+                    <div className="inner-loader">
+                    </div>
+                </div> 
+            </div>
+        )
+    }
+    
 }
 
 
